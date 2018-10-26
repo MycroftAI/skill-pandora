6476513a876558f45abe16c437951ef4d112d83f
dewBashir <44285697+dewBashir@users.noreply.github.com>
2018-10-27 00:44:38 +0530

    Make change

--- requirements.sh
+++ requirements.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-found_exe() {
+found_exe() 
+{
     hash "$1" 2>/dev/null
 }
 
 # On a Mark 1 the installation process is often running under a limited
-# user named 'mycroft'.  So avoid apt-get for installing packages.
+# user named 'mycroft'. So avoid apt-get for installing packages.
 
 # polkit uses pkcon instead of apt-get; pkcon will then run apt-get
 if found_exe pkcon; then
